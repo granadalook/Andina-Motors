b6ffc38bd91eec99fad5e380ec628a5020cccf4c
granadalook <Tiven17.jsga@gmail.com>
2021-07-13 22:42:19 -0500

    feature/ajustes_js:se crea js y se crean objetos y fn

---
+++ autos.js
@@ -0,0 +1,81 @@
+class autos {
+  constructor(marca, peso, potencia, color, modelo) {
+    this.marca = marca;
+    this.peso = peso;
+    this.potencia = potencia;
+    this.color = color;
+    this.modelo = modelo;
+  }
+
+  mostrasAutos() {
+    document.write(
+      "Este auto es un " +
+        this.marca +
+        " de color " +
+        this.color +
+        " tiene un peso de " +
+        this.peso +
+        " kilos y cuenta con " +
+        this.potencia +
+        " caballos de fuerza fabricado en el año " +
+        this.modelo +
+        "<br>"
+    );
+  }
+
+  pesopoten() {
+    let relacion = this.peso / this.potencia;
+    document.write(
+      "  la relacion peso potencia del " +
+        this.marca +
+        " es de " +
+        relacion +
+        " kl/hp" +
+        "<br><br>"
+    );
+    if (relacion > 0 && relacion < 1) {
+      alert(
+        "El " +
+          this.marca +
+          " cuenta con una relacion peso potencia menor a 1  exactamente  de " +
+          relacion +
+          " lo cual lo hace uno de los autos  mas rapidos del mundo"
+      );
+    } else if (relacion > 2.44 && relacion < 3) {
+      alert(
+        "El " +
+          this.marca +
+          " cuenta con una relacion mayor a 2 exactamente de " +
+          relacion +
+          "  lo que lo hace uno de los mas lentos del grupo"
+      );
+    }
+  }
+}
+let crearCoche = false;
+let carros = [];
+do {
+  let respuesta = prompt("Quieres crear un coche");
+  if (respuesta == "si") {
+    crearCoche = true;
+    let marca = prompt(" Marca de tu auto");
+    let color = prompt(" Color de tu auto");
+    let modelo = prompt(" Modelo de tu auto");
+    let potencia = prompt(" Potencia de tu auto en HP");
+    let peso = prompt(" Peso de tu auto KL");
+
+    carros.push(new autos(marca, peso, potencia, color, modelo));
+  } else {
+    crearCoche = false;
+  }
+} while (crearCoche);
+
+function mostrar() {
+  for (let index = 0; index < carros.length; index++) {
+    carros[index].mostrasAutos();
+
+    carros[index].pesopoten();
+  }
+}
+
+mostrar();
