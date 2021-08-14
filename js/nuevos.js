adbaf7f60ae690efe822fb2e4897c56351c7d3eb
granadalook <Tiven17.jsga@gmail.com>
2021-08-14 15:03:02 -0500

    feature/ajustes_js:se crea vista nuevos con json local  css y html jquery

--- js/nuevos.js
+++ js/nuevos.js
@@ -1,25 +1,29 @@
+const URLJSON = "../data/data.json";
 
-
-const URLJSON = "../data/data.json"
-
-
-
-$("#btn1").click(() => { 
+$("#btn1").click(() => {
   $.getJSON(URLJSON, function (respuesta, estado) {
-      if(estado === "success"){
-        let misDatos = respuesta;
-        for (const dato of misDatos) {
-            $("section").append(`<article class="article--card">
-                                    <h1>${dato.id}</h1>
-                                    <h3>${dato.marca}</h3>
-                                    <h3>${dato.peso}</h3>
-                                    <h3>${dato.potencia}</h3>
-                                    <h3>${dato.color}</h3>
-                                    <h3>${dato.modelo}</h3>
-                                    <button>SABER MAS </button>
-                                    <br><hr>
-                                    </article>`)
-        }  
- }
- });
+    if (estado === "success") {
+      let misDatos = respuesta;
+      for (const dato of misDatos) {
+        $("section").append(`<article class="article--card">
+                                  <div class="article--card--divtex">
+                                    <h1> Auto: ${dato.id}</h1>
+                                    <h3> Marca: ${dato.marca}</h3>
+                                    <h3>Modelo: ${dato.modelo}</h3>
+                                    <h3>Color: ${dato.color}</h3>
+                                    <h3> Peso: ${dato.peso} Kl</h3>
+                                    <h3>potencia: ${dato.potencia} Hp</h3>
+                                    
+                                    
+                                    <br>
+                                    <button><a href="${dato.url}">SABER MAS DE LA MARCA</a> </button>
+                                    
+                                  </div>
+                                  <div class="article--card--divImg">
+                                  <img src="${dato.logo}" alt="${dato.marca}">
+                                  </div>
+                                    </article>`);
+      }
+    }
+  });
 });
